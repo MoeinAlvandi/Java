9ed3520691fda2f3e7d0a7643d3ba61022fb260f
Moein Alvandi <mr.programmer1398@gmail.com>
2023-08-07 12:23:22 +0330

    Add files via upload

---
+++ Introduction_Java_Programming/IsAhasASample/User.java
@@ -0,0 +1,34 @@
+public class User{
+	private int ID;
+	private String FullName;
+	private String Email;
+	
+	public void setID(int ID)
+	{
+		this.ID=ID;
+	}
+	public int getID()
+	{
+		return ID;
+	}
+	
+	public void setFullName(String Fullname)
+	{
+		this.FullName=FullName;
+	}
+	public String getFullName()
+	{
+		return FullName;
+	}
+	
+	public String setEmail(String Email)
+	{
+		this.Email=Email;
+	}
+	public String getEmail()
+	{
+		return Email;
+	}
+	
+	
+}
